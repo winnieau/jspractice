181e9266f00e9c86c52039f0901f5ad849419b20
Ajitsy <sanghera.p@gmail.com>
2016-02-10 14:31:46 +0000

    says Java, Buzz or JavaBuzz if multiple of 3, 5 or 15 respectively

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -32,4 +32,24 @@ describe('JavaBuzz', function(){
 
   });
 
+  describe('when playing, says', function() {
+
+    it('"Java" when a number is divisible by 3', function(){
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function(){
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"JavaBuzz" when a number is divisible by 15', function(){
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
+
+    it('the number provided if not divisible by 3, 5 or 15', function(){
+      expect(javabuzz.says(4)).toEqual(4);
+    });
+
+  });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,13 +1,30 @@
 var Javabuzz = function(){};
 
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
 Javabuzz.prototype.isDivisibleByThree = function (number) {
-  return (number % 3 === 0);
+  return this._isDivisibleBy(number, 3);
 };
 
 Javabuzz.prototype.isDivisibleByFive = function (number) {
-  return (number % 5 === 0);
+  return this._isDivisibleBy(number, 5);
 };
 
-Javabuzz.prototype.isDivisibleByFifteen = function (number) {
-  return (number % 15 === 0);
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
+
+Javabuzz.prototype.says = function (number) {
+ if (this.isDivisibleByFifteen(number)) {
+   return "JavaBuzz";
+ }
+ if (this.isDivisibleByThree(number)) {
+   return "Java";
+ }
+ if (this.isDivisibleByFive(number)) {
+   return "Buzz";
+ }
+ return number;
 };
